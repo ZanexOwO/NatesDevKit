e8a5e6a0ab1d6f474435b9b8a8e5fe4fcb5aab8e
Zanex <46333540+ZanexOwO@users.noreply.github.com>
2019-02-19 10:16:58 +0000

    Hello World :)
    First commit :100:

---
+++ NatesLibary/GenerateSarcasm.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace NatesLibary
+{
+    public class GenerateSarcasm
+    {
+
+
+    }
+}
---
+++ NatesLibary/HardwareInfo.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Management;
+
+
+namespace NatesLibary
+{
+    public class HardwareInfo
+    {
+        /// <summary>
+        /// Get the local user Computer Name
+        /// </summary>
+        /// <returns></returns>
+        public static string ComputerName()
+        {
+            ManagementClass Mc = new ManagementClass("Win32_ComputerSystem");
+            ManagementObjectCollection Moc = Mc.GetInstances();
+            string Name = string.Empty;
+            foreach (ManagementObject Mo in Moc)
+            {
+                Name = (string)Mo["Name"];
+            }
+            return Name;
+        }
+        /// <summary>
+        /// Gets the users account name.
+        /// </summary>
+        /// <returns></returns>
+        public static string AccountName()
+        {
+            ManagementObjectSearcher MCSearcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_UserAccount");
+            foreach (ManagementObject wmi in MCSearcher.Get())
+            {
+                try
+                {
+                    return wmi.GetPropertyValue("Name").ToString();
+                }
+                catch { }
+            }
+            return "User Account Name: Unknown";
+        }
+        /// <summary>
+        /// Gets the processor Id.
+        /// </summary>
+        /// <returns></returns>
+        public static string ProcessorId()
+        {
+            ManagementClass Mc = new ManagementClass("Win32_Processor");
+            ManagementObjectCollection Moc = Mc.GetInstances();
+            string Id = String.Empty;
+            foreach (ManagementObject Mo in Moc)
+            {
+                Id = Mo.Properties["ProcessorID"].Value.ToString();
+                break;
+            }
+            return Id;
+
+        }
+
+        /// <summary>
+        /// Retrieving No of Ram Slot on Motherboard.
+        /// </summary>
+        /// <returns></returns>
+        public static string GetNoRamSlots()
+        {
+            int MemSlots = 0;
+            ManagementScope oMs = new ManagementScope();
+            ObjectQuery oQuery2 = new ObjectQuery("SELECT MemoryDevices FROM Win32_PhysicalMemoryArray");
+            ManagementObjectSearcher oSearcher2 = new ManagementObjectSearcher(oMs, oQuery2);
+            ManagementObjectCollection oCollection2 = oSearcher2.Get();
+            foreach (ManagementObject obj in oCollection2)
+            {
+                MemSlots = Convert.ToInt32(obj["MemoryDevices"]);
+            }
+            return MemSlots.ToString();
+        }
+
+        //Get CPU Temprature.
+        /// <summary>
+        /// method for retrieving the CPU Manufacturer
+        /// using the WMI class
+        /// </summary>
+        /// <returns>CPU Manufacturer</returns>
+        public static string GetCPUManufacturer()
+        {
+            string cpuMan = String.Empty;
+            //create an instance of the Managemnet class with the
+            //Win32_Processor class
+            ManagementClass mgmt = new ManagementClass("Win32_Processor");
+            //create a ManagementObjectCollection to loop through
+            ManagementObjectCollection objCol = mgmt.GetInstances();
+            //start our loop for all processors found
+            foreach (ManagementObject obj in objCol)
+            {
+                if (cpuMan == String.Empty)
+                {
+                    // only return manufacturer from first CPU
+                    cpuMan = obj.Properties["Manufacturer"].Value.ToString();
+                }
+            }
+            return cpuMan;
+        }
+
+        /// <summary>
+        /// method to retrieve the CPU's current
+        /// clock speed using the WMI class
+        /// </summary>
+        /// <returns>Clock speed</returns>
+        public static int GetCPUCurrentClockSpeed()
+        {
+            int cpuClockSpeed = 0;
+            //create an instance of the Managemnet class with the
+            //Win32_Processor class
+            ManagementClass mgmt = new ManagementClass("Win32_Processor");
+            //create a ManagementObjectCollection to loop through
+            ManagementObjectCollection objCol = mgmt.GetInstances();
+            //start our loop for all processors found
+            foreach (ManagementObject obj in objCol)
+            {
+                if (cpuClockSpeed == 0)
+                {
+                    // only return cpuStatus from first CPU
+                    cpuClockSpeed = Convert.ToInt32(obj.Properties["CurrentClockSpeed"].Value.ToString());
+                }
+            }
+            //return the status
+            return cpuClockSpeed;
+        }
+
+        /// <summary>
+        /// method to retrieve the network adapters
+        /// default IP gateway using WMI
+        /// </summary>
+        /// <returns>adapters default IP gateway</returns>
+        public static string GetDefaultIPGateway()
+        {
+            //create out management class object using the
+            //Win32_NetworkAdapterConfiguration class to get the attributes
+            //of the network adapter
+            ManagementClass mgmt = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            //create our ManagementObjectCollection to get the attributes with
+            ManagementObjectCollection objCol = mgmt.GetInstances();
+            string gateway = String.Empty;
+            //loop through all the objects we find
+            foreach (ManagementObject obj in objCol)
+            {
+                if (gateway == String.Empty)  // only return MAC Address from first card
+                {
+                    //grab the value from the first network adapter we find
+                    //you can change the string to an array and get all
+                    //network adapters found as well
+                    //check to see if the adapter's IPEnabled
+                    //equals true
+                    if ((bool)obj["IPEnabled"] == true)
+                    {
+                        gateway = obj["DefaultIPGateway"].ToString();
+                    }
+                }
+                //dispose of our object
+                obj.Dispose();
+            }
+            //replace the ":" with an empty space, this could also
+            //be removed if you wish
+            gateway = gateway.Replace(":", "");
+            //return the mac address
+            return gateway;
+        }
+
+        /// <summary>
+        /// Retrieve CPU Speed.
+        /// </summary>
+        /// <returns></returns>
+        public static double? GetCpuSpeedInGHz()
+        {
+            double? GHz = null;
+            using (ManagementClass mc = new ManagementClass("Win32_Processor"))
+            {
+                foreach (ManagementObject mo in mc.GetInstances())
+                {
+                    GHz = 0.001 * (UInt32)mo.Properties["CurrentClockSpeed"].Value;
+                    break;
+                }
+            }
+            return GHz;
+        }
+
+        /// <summary>
+        /// Retrieving Current Language
+        /// </summary>
+        /// <returns></returns>
+        public static string GetCurrentLanguage()
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_BIOS");
+
+            foreach (ManagementObject wmi in searcher.Get())
+            {
+                try
+                {
+                    return wmi.GetPropertyValue("CurrentLanguage").ToString();
+                }
+                catch { }
+            }
+
+            return "BIOS Maker: Unknown";
+        }
+
+        /// <summary>
+        /// Retrieving Current Language.
+        /// </summary>
+        /// <returns></returns>
+        public static string GetOSInformation()
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_OperatingSystem");
+            foreach (ManagementObject wmi in searcher.Get())
+            {
+                try
+                {
+                    return ((string)wmi["Caption"]).Trim() + ", " + (string)wmi["Version"] + ", " + (string)wmi["OSArchitecture"];
+                }
+                catch { }
+            }
+            return "BIOS Maker: Unknown";
+        }
+
+        /// <summary>
+        /// Retrieving Processor Information.
+        /// </summary>
+        /// <returns></returns>
+        public static String GetProcessorInformation()
+        {
+            ManagementClass mc = new ManagementClass("win32_processor");
+            ManagementObjectCollection moc = mc.GetInstances();
+            String info = String.Empty;
+            foreach (ManagementObject mo in moc)
+            {
+                string name = (string)mo["Name"];
+                name = name.Replace("(TM)", "™").Replace("(tm)", "™").Replace("(R)", "®").Replace("(r)", "®").Replace("(C)", "©").Replace("(c)", "©").Replace("    ", " ").Replace("  ", " ");
+
+                info = name + ", " + (string)mo["Caption"] + ", " + (string)mo["SocketDesignation"];
+                //mo.MajestixAutoClicker.Properties["Name"].Value.ToString();
+                //break;
+            }
+            return info;
+        }
+
+
+
+
+
+    }
+}
